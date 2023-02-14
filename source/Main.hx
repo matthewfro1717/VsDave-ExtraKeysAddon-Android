e2798f5d2de78ad56082796488a20e8a446be4c2
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-02-14 12:01:23 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -74,7 +74,7 @@ class Main extends Sprite
 		}
 
 		initialState = StartStateSelector;
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, skipSplash, startFullscreen));
 
 		#if !mobile
 		fps = new FpsDisplay(10, 3, 0xFFFFFF);
