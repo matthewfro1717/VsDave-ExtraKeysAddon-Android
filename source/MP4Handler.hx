05b533cc59d79c6dd70b4e5761732f6f6a116f06
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-02-14 12:06:21 -0500

    Create MP4Handler.hx

---
+++ source/MP4Handler.hx
@@ -0,0 +1,27 @@
+package;
+
+#if VIDEOS_ALLOWED
+import vlc.MP4Handler;
+#end
+
+class VideoState extends MusicBeatState
+{
+  override function create():Void
+  {
+    #if VIDEOS_ALLOWED
+    var video:MP4Handler = new MP4Handler();
+    video.playVideo(Paths.video('yourVideoName'));
+    video.finishCallback = function()
+    {
+      MusicBeatState.switchState(new TitleState());
+    };
+    #else
+    MusicBeatState.switchState(new TitleState());
+    #end
+  }
+
+  override function update(elapsed:Float):Void
+  {
+    super.update(elapsed);
+  }
+}
