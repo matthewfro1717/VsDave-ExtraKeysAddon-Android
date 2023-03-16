60e6d023b64b8eaa1f95983b5e16a3bbaa9a8413
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-16 19:13:24 -0400

    Update StartStateSelector.hx

--- source/StartStateSelector.hx
+++ source/StartStateSelector.hx
@@ -7,6 +7,10 @@ class StartStateSelector extends FlxState
 {
    public override function create()
    {
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
+
       LanguageManager.initSave();
       LanguageManager.save.data.language == null;
       if (LanguageManager.save.data.language == null)
